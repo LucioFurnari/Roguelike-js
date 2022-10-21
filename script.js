7ecb4d6c2c164f0de9562ce75bf32582de86684c
LucioFurnari <luciofurnari99@gmail.com>
2022-10-21 13:27:07 -0300

    Change the detect objet function

--- script.js
+++ script.js
@@ -3,16 +3,20 @@ let tileData = {
     terrain:"",
     wall:"||"
 }
+let iRow = 9;
+let jRow = 9;
+let iCol = 7;
+let jCol = 7;
 
 let Tiles = [
     ["","","","","","","","","",""," "," ","","","","","","","","","","","","",],
     ["","","","","","","","","","","","","","","","","","","","","","","","",],
     ["","","","","","","","","","","","","","","","","","","","","","","","",],
-    ["","","","","","","","","","","","","","","","","","","","","","","","",],
-    ["","","","","","","","","","","","","","","","","","","","","","","","",],
-    ["","","","","","","","","","","","","","","","","","","","","","","","",],
-    ["","","","","","","","||","","","","","","","","","","","","","","","","",],
-    ["","","","","","","","","","","","","","","","","","","","","","","","",],
+    ["","","","","","","","","","","","","","||","","","","","","","","","","",],
+    ["","","","","","","","","","","","","","||","","","","","","","","","","",],
+    ["","","","","","","","","","","","","","||","","","","","","","","","","",],
+    ["","","","","","","","||","","","","","","||","","","","","","","","","","",],
+    ["","","","","","","","","","","","","","||","","","","","","","","","","",],
     ["||","||","||","||","||","","","T","","||","||","||","||","||","||","||","||","||","||","||","||","||","||","||",],
     ["","","","","","","","@","","","","","","","","","","","","","","","","",],
     ["","","","","","","","","","","","","","","","","","","","","","","","",],
@@ -41,81 +45,59 @@ function createTiles() {
             let tileRow = document.createElement("div");
             tileRow.classList.add("tile");
             tileRow.textContent = row;
+            // if(row == "||"){
+            //     tileRow.style.backgroundColor ="red"
+            // }
             MapContainer.appendChild(tileRow);
         })
     })
 }
 
 createTiles()
-
 window.addEventListener("keydown",(event) => moveCharacter(event))
 
 
-
-let iRow = 9;
-let jRow = 9;
-let iCol = 7;
-let jCol = 7;
-
-// console.log(Tiles[6][7]);
 function moveCharacter(e){
     console.log(e.key);
     switch (e.key) {
         case "ArrowUp":
-            console.log(iRow);
-            // if(!detectObjet(iRow,iCol)){
             jRow = jRow-1;
-            if(Tiles[jRow] == undefined){
+            if(detectObjet(jRow,jCol)){
                 jRow = iRow;
             } else {
                 [Tiles[iRow][iCol],Tiles[jRow][iCol]] = [Tiles[jRow][iCol],Tiles[iRow][iCol]];
-                let colorTile = MapContainer.children;
-                console.log(colorTile);
-                colorTile.style.backgroundColor = "red"
                 iRow = iRow-1;
             }
-            // iCol= 7;
-            console.log(iRow);
             createTiles();
-            // } else {
-                return
-            
             break;
         case "ArrowDown":
             console.log(jRow);
-            
-            // if(!detectObjet(jRow,7)){
             jRow = jRow + 1;
-            if(Tiles[jRow] == undefined){
+            if(detectObjet(jRow,jCol)){
                 jRow = iRow;
             } else {
-            [Tiles[iRow][iCol],Tiles[jRow][iCol]] = [Tiles[jRow][iCol],Tiles[iRow][iCol]];
-            iRow = iRow+1;
+                [Tiles[iRow][iCol],Tiles[jRow][iCol]] = [Tiles[jRow][iCol],Tiles[iRow][iCol]];
+                iRow = iRow+1;
             }
-            
             createTiles();
-        
-            // else {
-            //     return
-            // };
             break;
         case "ArrowRight":
             jCol = jCol + 1;
-            if(Tiles[jCol] == undefined){
+            if(detectObjet(jRow,jCol)){
                 jCol = iCol;
             } else {
-            [Tiles[iRow][iCol],Tiles[jRow][jCol]] = [Tiles[jRow][jCol],Tiles[iRow][iCol]];
-            iCol = iCol+1;
+                [Tiles[iRow][iCol],Tiles[jRow][jCol]] = [Tiles[jRow][jCol],Tiles[iRow][iCol]];
+                iCol = iCol+1;
             }
             createTiles();
             break;
         case "ArrowLeft":
             jCol = jCol - 1;
-            if(Tiles[jCol] == undefined){
+            if(detectObjet(jRow,jCol)){
                 jCol = iCol;
             } else {
-            [Tiles[iRow][iCol],Tiles[jRow][jCol]] = [Tiles[jRow][jCol],Tiles[iRow][iCol]];
-            iCol = iCol-1;
+                [Tiles[iRow][iCol],Tiles[jRow][jCol]] = [Tiles[jRow][jCol],Tiles[iRow][iCol]];
+                iCol = iCol-1;
             }
             createTiles();
             break;
@@ -126,10 +108,12 @@ function moveCharacter(e){
 
 function detectObjet(row,column) {
     switch (true) {
-        case Tiles[row][column] == "||":
+        case Tiles[row] == undefined:
+            return true
+        case Tiles[row][column] == undefined:
             return true;
-        // case Tiles[row][column] == "T": 
-        //     return false;
+        case Tiles[row][column] == "||":
+            return true
         default:
             break;
     }
